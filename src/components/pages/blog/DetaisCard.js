85795610b7968e04316d6ac1cba73cf0395a0c63
Tofayel <mdtofayelhossain69@gmail.com>
2023-03-07 00:14:59 +0600

    blog design added

--- src/components/pages/blog/DetaisCard.js
+++ src/components/pages/blog/DetaisCard.js
@@ -5,9 +5,9 @@ const DetaisCard = ({cardData}) => {
     console.log(cardData);
     const {pic,ques,details}=cardData;
     return (
-        <div className='bg-zinc-800 m-4 text-white p-8'>
-            <img className='object-cover h-48 w-96 mx-auto mb-4' src={pic} alt="pic" />
-            <p className='text-2xl '>{ques}</p>
+        <div className='bg-zinc-800 m-4 text-white p-8 duration-1000 hover:scale-105 hover:border-4 '>
+            <img className='object-cover h-48 w-96 mx-auto mb-4 ' src={pic} alt="pic" />
+            <p className='text-2xl text-amber-600'>{ques}</p>
             <p className='my-3'>{details}</p>
         </div>
     );
