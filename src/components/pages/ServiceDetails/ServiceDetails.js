edfa56134e58af28d9a876f64787fdbb9ff76156
Tofayel <mdtofayelhossain69@gmail.com>
2023-03-20 00:57:45 +0600

    review object post to database added

--- src/components/pages/ServiceDetails/ServiceDetails.js
+++ src/components/pages/ServiceDetails/ServiceDetails.js
@@ -1,22 +1,64 @@
-import React from 'react';
+
 import {useLoaderData } from 'react-router-dom';
 import { BsStarFill } from 'react-icons/bs';
 import { BsStarHalf } from 'react-icons/bs';
 import ReviewCard from './ReviewCard';
 
-const ServiceDetails = () => {
 
+
+// rating 
+
+import ReactStars from "react-rating-stars-component";
+import { useState } from 'react';
+ 
+
+
+
+
+ 
+
+
+
+
+
+
+
+const ServiceDetails = () => {
     const singleServiceDetailsData = useLoaderData()
-    console.log(singleServiceDetailsData);
+    // console.log(singleServiceDetailsData);
+    const [rating,setRating] = useState()
 
     const handleCancelBtn=()=>{
         let review = document.getElementById('inputfield');
         review.value = ''
-        
     }
+    const ratingChanged = (newRating) => {setRating(newRating)};
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        const reviewerName = event.target.reviewerName.value;
+        const reviewTitle = event.target.reviewTitle.value;
+        const reviewDetails = event.target.reviewDetailsField.value;
+       
+        const reviewObj = {
+            serviceId: singleServiceDetailsData._id ,
+            reviewerName,
+            reviewTitle,
+            reviewDetails,
+            rating
+        }
+        //  console.log(reviewObj);
+
+
+            fetch('http://localhost:5000/serviceReview',{
+                method:'POST',
+                headers:{"content-type":"application/json"},
+                body:JSON.stringify(reviewObj)
+            })
+            .then(res => res.json())
+            .then(data => console.log(data))
+
     }
 
 
@@ -49,15 +91,29 @@ const ServiceDetails = () => {
                 {/*submit review section */}
                 <div className='m-10'>
                     <span className='text-left block font-bold'>Write Your Review</span>
+
+
+                    {/* give Your rating  */}
+                    <ReactStars
+                            count={5}
+                            onChange={ratingChanged}
+                            size={24}
+                            isHalf={true}
+                            emptyIcon={<i className="far fa-star"></i>}
+                            halfIcon={<i className="fa fa-star-half-alt"></i>}
+                            fullIcon={<i className="fa fa-star"></i>}
+                            activeColor="#ffd700"
+                        />,
+
+
                     <form action="" onSubmit={handleSubmit}>
-                            <input type="text" placeholder="Your Name" className="input input-bordered w-3/12" />
-                            <input type="text" placeholder="Write review Title" className="input input-bordered w-3/4" />
-                            
-                            <textarea name='reviewField' id='inputfield' className="border-2 rounded textarea-lg w-full mt-3" placeholder="write your detail review here ."></textarea>
+                            <input name='reviewerName' type="text" placeholder="Your Name" className="input input-bordered w-3/12" />
+                            <input name='reviewTitle' type="text" placeholder="Write review Title" className="input input-bordered w-3/4" />
+                            <textarea name='reviewDetailsField' id='inputfield' className="border-2 rounded textarea-lg w-full mt-3" placeholder="write your detail review here ."></textarea>
 
                             <div className='text-right'>
-                            <button onClick={handleCancelBtn} className=' btn bg-white text-black mr-4'>Clear</button>
-                            <button className='btn'>Submit</button>
+                            <p onClick={handleCancelBtn} className=' btn bg-white text-black mr-4'>Clear</p>
+                            <button value='submit' className='btn'>Submit</button>
                             </div>
                     </form>
                 </div>
